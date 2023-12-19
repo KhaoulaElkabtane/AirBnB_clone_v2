b5493cdc69624d9503787cae138162fe6ea79b7a
KhaoulaElkabtane <132408788+KhaoulaElkabtane@users.noreply.github.com>
2023-12-19 13:35:09 +0100

    review.py

--- models/review.py
+++ models/review.py
@@ -1,18 +1,35 @@
 #!/usr/bin/python3
-"""This is the review class"""
+"""This is the state class"""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+import models
+from models.city import City
+import shlex
 
 
-class Review(BaseModel, Base):
-    """This is the class for Review
+class State(BaseModel, Base):
+    """This is the class for State
     Attributes:
-        place_id: place id
-        user_id: user id
-        text: review description
+        name: input name
     """
-    __tablename__ = "reviews"
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
+
+    @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
